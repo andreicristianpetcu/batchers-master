d4f19d82fb782ec24a8922ebcc7cebf35aa6304d
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-05 18:09:08 +0300

    karma jasmine and all the fun stuff

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,11 +1,16 @@
 # install nodejs
 rm -rf ~/.ndenv
 git clone https://github.com/riywo/ndenv ~/.ndenv
-# echo 'export PATH="$HOME/.ndenv/bin:$PATH"' >> ~/.bash_profile
-# echo 'eval "$(ndenv init -)"' >> ~/.bash_profile
-exec $SHELL -l
-mkdir -p ~/.ndenv/plugins/
+echo 'export PATH="$HOME/.ndenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(ndenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
 git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
 ndenv install v0.10.28
 ndenv global v0.10.28
 ndenv rehash
+
+npm install -g karma-ng-scenario karma-junit-reporter
+npm install -g karma-jasmine
+npm install karma-chrome-launcher --save-dev
+npm install karma-firefox-launcher --save-dev
+npm install karma-jasmine --save-dev
