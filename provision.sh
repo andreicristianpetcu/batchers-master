1f9ccc30ff82eb65dfcb1a774b1aa8af447037b7
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-05 14:03:16 +0300

    installed Oracle JDK 8

--- provision.sh
+++ provision.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-apt-get install nmap -y
+add-apt-repository ppa:webupd8team/java
+apt-get update
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get install oracle-java8-installer -y
