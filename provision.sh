47cd5fdeadc0f1ef5779e7a835455157949e59da
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-05 14:22:30 +0300

    added myslq and maven

--- provision.sh
+++ provision.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-add-apt-repository ppa:webupd8team/java
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get install oracle-java8-installer -y
-apt-get -y install oracle-java8-set-default
+
+# install java
+# add-apt-repository ppa:webupd8team/java
+# echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+# apt-get install oracle-java8-installer -y
+# apt-get install oracle-java8-set-default -y
+apt-get install maven
+
+# install mysql server
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password taxcalculator'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password taxcalculator'
+
+apt-get install mysql-server-5.5 -y
