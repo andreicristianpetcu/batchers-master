02e1c677b5c419ab72cc077e86a7513076fb2312
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-05 14:04:03 +0300

    set default java8

--- provision.sh
+++ provision.sh
@@ -3,3 +3,4 @@ add-apt-repository ppa:webupd8team/java
 apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 apt-get install oracle-java8-installer -y
+apt-get -y install oracle-java8-set-default
