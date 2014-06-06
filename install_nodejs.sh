ab034e4572e0fb63de0d06dc9ac98b6d1e7a77ee
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-06 15:34:27 +0300

    removed from comments all the ignored instructions

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,32 +1,32 @@
 # install nodejs
-# rm -rf ~/.ndenv
-# git clone https://github.com/riywo/ndenv ~/.ndenv
-# echo 'export PATH="$HOME/.ndenv/bin:$PATH"' >> ~/.bash_profile
-# echo 'eval "$(ndenv init -)"' >> ~/.bash_profile
-# source ~/.bash_profile
-# git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
-# ndenv install v0.10.28
-# ndenv global v0.10.28
-# ndenv rehash
-#
-
-# echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bash_profile
-# rm -rf ~/.local/lib/node_modules
-# echo "prefix = ~/.local
-# root = ~/.local/lib/node_modules
-# binroot = ~/.local/bin
-# manroot = ~/.local/share/man" | tee ~/.npmrc
-#
-# source ~/.npmrc
-
-# npm install -g karma karma-ng-scenario karma-junit-reporter karma-jasmine
-# npm install karma-chrome-launcher --save-dev
-# npm install karma-firefox-launcher -g
-# npm install karma-jasmine --save-dev
+rm -rf ~/.ndenv
+git clone https://github.com/riywo/ndenv ~/.ndenv
+echo 'export PATH="$HOME/.ndenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(ndenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
+ndenv install v0.10.28
+ndenv global v0.10.28
+ndenv rehash
 
 
-# rm -rf ~/.local/bin/karma
-# ln -s ~/.local/lib/node_modules/karma/bin/karma ~/.local/bin/karma
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bash_profile
+rm -rf ~/.local/lib/node_modules
+echo "prefix = ~/.local
+root = ~/.local/lib/node_modules
+binroot = ~/.local/bin
+manroot = ~/.local/share/man" | tee ~/.npmrc
+
+source ~/.npmrc
+
+npm install -g karma karma-ng-scenario karma-junit-reporter karma-jasmine
+npm install karma-chrome-launcher --save-dev
+npm install karma-firefox-launcher -g
+npm install karma-jasmine --save-dev
+
+
+rm -rf ~/.local/bin/karma
+ln -s ~/.local/lib/node_modules/karma/bin/karma ~/.local/bin/karma
 
 cd ~
 rm -rf batchers
--- provision.sh
+++ provision.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-# export DEBIAN_FRONTEND=noninteractive
-# apt-get update && apt-get upgrade -y
-# apt-get install git firefox xvfb htop -y
+export DEBIAN_FRONTEND=noninteractive
+apt-get update && apt-get upgrade -y
+apt-get install git firefox xvfb htop -y
 
-# # configure display for e2e
-# export DISPLAY=:10
-# killall Xcfb
-# Xvfb :10 -ac </dev/null &>/dev/null &
+# configure display for e2e
+export DISPLAY=:10
+killall Xcfb
+Xvfb :10 -ac </dev/null &>/dev/null &
 
-# # install java
-# add-apt-repository ppa:webupd8team/java
-# echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-# apt-get install oracle-java8-installer -y
-# apt-get install oracle-java8-set-default -y
-# apt-get install maven
+# install java
+add-apt-repository ppa:webupd8team/java
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+apt-get install oracle-java8-installer -y
+apt-get install oracle-java8-set-default -y
+apt-get install maven
 
 
-# # install mysql server
-# debconf-set-selections <<< 'mysql-server mysql-server/root_password password taxcalculator'
-# debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password taxcalculator'
-# apt-get install mysql-server-5.5 -y
+# install mysql server
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password taxcalculator'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password taxcalculator'
+apt-get install mysql-server-5.5 -y
 
 # set up mysql database
 
@@ -28,10 +28,6 @@
 chmod +x /home/vagrant/install_nodejs.sh 
 su -c "./install_nodejs.sh" -s /bin/bash vagrant
 
-# setup firefox for e2e?
-
 # set up rabbitmq
 
 # env variables needed?
-
-# tomcat 1, tomcat 2 for webservices
