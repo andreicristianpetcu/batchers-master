761a45ca5b5aef45b2d498005ada57b2efbe9b63
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-06 15:13:49 +0300

    install tomcat and jetty

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,34 +1,59 @@
 # install nodejs
-rm -rf ~/.ndenv
-git clone https://github.com/riywo/ndenv ~/.ndenv
-echo 'export PATH="$HOME/.ndenv/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(ndenv init -)"' >> ~/.bash_profile
-source ~/.bash_profile
-git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
-ndenv install v0.10.28
-ndenv global v0.10.28
-ndenv rehash
+# rm -rf ~/.ndenv
+# git clone https://github.com/riywo/ndenv ~/.ndenv
+# echo 'export PATH="$HOME/.ndenv/bin:$PATH"' >> ~/.bash_profile
+# echo 'eval "$(ndenv init -)"' >> ~/.bash_profile
+# source ~/.bash_profile
+# git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
+# ndenv install v0.10.28
+# ndenv global v0.10.28
+# ndenv rehash
+#
+
+# echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bash_profile
+# rm -rf ~/.local/lib/node_modules
+# echo "prefix = ~/.local
+# root = ~/.local/lib/node_modules
+# binroot = ~/.local/bin
+# manroot = ~/.local/share/man" | tee ~/.npmrc
+#
+# source ~/.npmrc
+
+# npm install -g karma karma-ng-scenario karma-junit-reporter karma-jasmine
+# npm install karma-chrome-launcher --save-dev
+# npm install karma-firefox-launcher -g
+# npm install karma-jasmine --save-dev
 
 
-echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bash_profile
-rm -rf ~/.local/lib/node_modules
-echo "prefix = ~/.local
-root = ~/.local/lib/node_modules
-binroot = ~/.local/bin
-manroot = ~/.local/share/man" | tee ~/.npmrc
+# rm -rf ~/.local/bin/karma
+# ln -s ~/.local/lib/node_modules/karma/bin/karma ~/.local/bin/karma
 
-source ~/.npmrc
-
-npm install -g karma karma-ng-scenario karma-junit-reporter karma-jasmine
-npm install karma-chrome-launcher --save-dev
-# npm install karma-firefox-launcher --save-dev
-npm install karma-firefox-launcher -g
-npm install karma-jasmine --save-dev
-
-rm -rf ~/.local/bin/karma
-ln -s ~/.local/lib/node_modules/karma/bin/karma ~/.local/bin/karma
-
+cd ~
 rm -rf batchers
 git clone https://github.com/cegeka/batchers.git
-cd batchers/taxcalculator
+cd ~/batchers/taxcalculator
 mvn clean install -Dmaven.test.skip=true
+
+cd taxcalculator-presentation
+mvn package
+# ~/batchers/taxcalculator/taxcalculator-presentation/target/taxcalculator-presentation-1.0-SNAPSHOT.war
+
+cd ../taxcalculator-stubwebservice/
+# kill jetty if exists
+kill -9 `lsof -i:9091 -t`
+mvn jetty:run </dev/null &>/dev/null &
+
+# install tomcat
+cd ~
+rm -rf apache-tomcat-*
+wget http://apache.cu.be/tomcat/tomcat-7/v7.0.54/bin/apache-tomcat-7.0.54.tar.gz
+tar -xvf apache-tomcat-7.0.54.tar.gz
+
+
+#change tomcat port form 8080 to 9090
+sed -i 's/"8080"/"9090"/g' ~/apache-tomcat-7.0.54/conf/server.xml
+apache-tomcat-7.0.54/bin/catalina.sh start
+
+
+rm -rf ~/apache-tomcat-7.0.54/webapps/taxcalculator-*
+cp ~/batchers/taxcalculator/taxcalculator-presentation/target/taxcalculator-presentation-1.0-SNAPSHOT.war ~/apache-tomcat-7.0.54/webapps/taxcalculator.war
