928301c5d41447f53a7f5d229b564eecbce15bbe
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-06 14:11:07 +0300

    made karma work

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -3,19 +3,31 @@
 # git clone https://github.com/riywo/ndenv ~/.ndenv
 # echo 'export PATH="$HOME/.ndenv/bin:$PATH"' >> ~/.bash_profile
 # echo 'eval "$(ndenv init -)"' >> ~/.bash_profile
-# source ~/.bash_profile
+source ~/.bash_profile
 # git clone https://github.com/riywo/node-build.git ~/.ndenv/plugins/node-build
 # ndenv install v0.10.28
 # ndenv global v0.10.28
 # ndenv rehash
 #
-# npm install -g karma-ng-scenario karma-junit-reporter
-# npm install -g karma-jasmine
-# npm install karma-chrome-launcher --save-dev
-# npm install karma-firefox-launcher --save-dev
-# npm install karma-jasmine --save-dev
 
-rm -rf batchers
-git clone https://github.com/cegeka/batchers.git
-cd batchers/taxcalculator
-mvn clean install
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bash_profile
+rm -rf ~/.local/lib/node_modules
+echo "prefix = ~/.local
+root = ~/.local/lib/node_modules
+binroot = ~/.local/bin
+manroot = ~/.local/share/man" | tee ~/.npmrc
+
+source ~/.npmrc
+
+npm install -g karma karma-ng-scenario karma-junit-reporter karma-jasmine
+npm install karma-chrome-launcher --save-dev
+npm install karma-firefox-launcher --save-dev
+npm install karma-jasmine --save-dev
+
+rm -rf ~/.local/bin/karma
+ln -s ~/.local/lib/node_modules/karma/bin/karma ~/.local/bin/karma
+
+# rm -rf batchers
+# git clone https://github.com/cegeka/batchers.git
+# cd batchers/taxcalculator
+# mvn clean install -Dmaven.test.skip=true
