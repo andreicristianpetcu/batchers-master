af79fb4d5120ae932d4f6ede7f2b79bb6e927658
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-06 15:46:20 +0300

    sudo !!

--- provision.sh
+++ provision.sh
@@ -12,15 +12,15 @@ Xvfb :10 -ac </dev/null &>/dev/null &
 # install java
 add-apt-repository ppa:webupd8team/java
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-apt-get install oracle-java8-installer -y
-apt-get install oracle-java8-set-default -y
-apt-get install maven
+sudo apt-get install oracle-java8-installer -y
+sudo apt-get install oracle-java8-set-default -y
+sudo apt-get install maven
 
 
 # install mysql server
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password taxcalculator'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password taxcalculator'
-apt-get install mysql-server-5.5 -y
+sudo apt-get install mysql-server-5.5 -y
 
 # set up mysql database
 
