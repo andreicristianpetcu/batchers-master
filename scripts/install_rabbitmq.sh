c2c87bc355f9e0a9fa5533940580ed0de0bf7743
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-11 17:04:06 +0300

    [Radu, Andrei] rabbitmq users

--- scripts/install_rabbitmq.sh
+++ scripts/install_rabbitmq.sh
@@ -8,8 +8,8 @@ apt-get update
 apt-get install rabbitmq-server -y
 service rabbitmq-server start
 rabbitmq-plugins enable rabbitmq_management
-rabbitmqctl add_user admin password
-#rabbitmqctl set_user_tags admin administrator
-#rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"
-#rabbitmqctl delete_user guest
+rabbitmqctl add_user batchers batchers
+rabbitmqctl set_user_tags batchers administrator
+rabbitmqctl set_permissions -p / batchers ".*" ".*" ".*"
+rabbitmqctl delete_user guest
 service rabbitmq-server restart
