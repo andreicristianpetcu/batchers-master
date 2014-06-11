94680c44072538df7b15d8a33bf600211ce7293e
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-11 17:13:28 +0300

    [Radu, Andrei] mysql available from lan

--- scripts/install_mysql.sh
+++ scripts/install_mysql.sh
@@ -7,4 +7,8 @@ sudo apt-get install mysql-server-5.5 -y
 
 mysql --user=root --password=taxcalculator -e "drop database if exists taxcalculator;"
 mysql --user=root --password=taxcalculator -e "create database taxcalculator;"
-mysql --user=root --password=taxcalculator -e "grant all on *.* to 'taxcalculator'@'localhost' identified by 'taxcalculator';"
+mysql --user=root --password=taxcalculator -e "grant all on *.* to 'taxcalculator'@% identified by 'taxcalculator';"
+
+cp /etc/mysql/my.cnf /etc/mysql/my.cnf.original
+sed -i 's/bind-address.*/bind-address            = 0.0.0.0/g' /etc/mysql/my.cnf
+service mysql restart
