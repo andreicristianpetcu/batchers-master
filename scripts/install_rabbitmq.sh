f308d3a777a435ac8052761045de0b33c6fbdb14
Andrei Cristian Petcu <Andrei.Petcu@cegeka.com>
2014-06-12 10:21:44 +0300

    [Andrei] removed rabbitmq web site since it gives 404 from time to time

--- scripts/install_rabbitmq.sh
+++ scripts/install_rabbitmq.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
-sudo apt-key add rabbitmq-signing-key-public.asc
-apt-get update
+# echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
+# sudo apt-key add rabbitmq-signing-key-public.asc
+# apt-get update
 apt-get install rabbitmq-server -y
 service rabbitmq-server start
 rabbitmq-plugins enable rabbitmq_management
